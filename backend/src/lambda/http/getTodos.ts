42cdf7bd075bb06571cdb022c7ce3a9f29550751
tranquochuy2210 <75486565+tranquochuy2210@users.noreply.github.com>
2022-06-08 19:56:45 +0700

    fix get todo

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,29 +1,29 @@
-import "source-map-support/register";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import * as middy from "middy";
-import { cors, httpErrorHandler } from "middy/middlewares";
+import 'source-map-support/register'
 
-import { createAttachmentPresignedUrl } from "../../helpers/todos";
-import { getUserId } from "../utils";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
 
+import { getAllTodos } from '../../helpers/todos'
+import { getUserId } from '../utils'
+
+// TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId;
-    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    let userId = getUserId(event);
-    const url = await createAttachmentPresignedUrl(todoId, userId);
+    // Write your code here
+    const userId = getUserId(event)
+    const todos = await getAllTodos(userId)
 
     return {
-      statusCode: 201,
+      statusCode: 200,
       body: JSON.stringify({
-        uploadUrl: url,
-      }),
-    };
+        items: todos
+      })
+    }
   }
-);
-
+)
 handler.use(httpErrorHandler()).use(
   cors({
-    credentials: true,
+    credentials: true
   })
-);
+)
